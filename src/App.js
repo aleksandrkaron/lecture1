fb65b42f78e9a2d6e958fff960c5b1d4ec0bdbcc
aleksandrkaron <66442545+aleksandrkaron@users.noreply.github.com>
2024-11-19 18:00:01 +0000

    Added function to send advices by clicking on the button

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { useState } from "react";
+
 export default function App() {
+  const [advice, setAdvice] = useState("");
+  async function getAdvice() {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    setAdvice(data.slip.advice);
+  }
+
   return (
     <div>
-      <h1>Hello, World!</h1>
-      <button>Get advice</button>
+      <h1>{advice}</h1>
+      <button onClick={getAdvice}>Get advice</button>
     </div>
   );
 }
