f43b620c7961c727b7415c931bc009724e42af05
aleksandrkaron <66442545+aleksandrkaron@users.noreply.github.com>
2024-11-19 17:46:40 +0000

    Initialization/Hello World

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
-function App() {
-  return;
-  <div></div>;
+export default function App() {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <button>Get advice</button>
+    </div>
+  );
 }
